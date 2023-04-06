16d55fe0f9f4bad5cdf9e1cfdbdf9db6e2e10f69
SuosPhearith <suosphearith@gmail.com>
2023-04-06 19:53:45 +0700

    First commit

---
+++ src/App.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        stage.setTitle("Welcome to Library App");
+        stage.setScene(new Scene(root, 500, 500));
+        stage.show();
+    }
+}
